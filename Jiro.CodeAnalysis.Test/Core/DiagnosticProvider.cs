f3d4856eb524cbb50a89617ec01ae23f961c1332
ryanmentzer <ryan@ryanmentzer.com>
2014-12-22 16:58:46 -0600

    refactoring

--- Jiro.CodeAnalysis.Test/Core/DiagnosticProvider.cs
+++ Jiro.CodeAnalysis.Test/Core/DiagnosticProvider.cs
@@ -3,6 +3,7 @@
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.Diagnostics;
     using System.Collections.Immutable;
+    using System.Diagnostics;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -10,26 +11,38 @@
     {
         private readonly Compilation compilation;
 
-        public DiagnosticProvider(Compilation compilation)
+        internal DiagnosticProvider(Compilation compilation)
         {
+            Debug.Assert(compilation != null, "compilation must not be null");
+
             this.compilation = compilation;
         }
 
-        public async Task<ImmutableArray<Diagnostic>> ListDiagnostics(params DiagnosticAnalyzer[] analyzers)
+        internal Task<ImmutableArray<Diagnostic>> ListDiagnostics(DiagnosticAnalyzer analyzer)
+        {
+            Debug.Assert(analyzer != null, "analyzer must not be null");
+
+            return this.ListDiagnostics(ImmutableArray.Create(analyzer));
+        }
+
+        internal Task<ImmutableArray<Diagnostic>> ListDiagnostics(ImmutableArray<DiagnosticAnalyzer> analyzers)
         {
             Compilation buffer = null;
 
             var driver =
                 AnalyzerDriver.Create(
                     this.compilation,
-                    analyzers.ToImmutableArray(),
+                    analyzers,
                     new AnalyzerOptions(ImmutableArray<AdditionalStream>.Empty, ImmutableDictionary<string, string>.Empty),
                     out buffer,
                     CancellationToken.None);
 
-            buffer.GetDiagnostics(CancellationToken.None);
+            if (buffer != null)
+            {
+                buffer.GetDiagnostics(CancellationToken.None);
+            }
 
-            return await driver.GetDiagnosticsAsync();
+            return driver.GetDiagnosticsAsync();
         }
     }
 }
---
+++ Jiro.CodeAnalysis.Test/Core/StringFixProvider.cs
@@ -0,0 +1,89 @@
+﻿namespace Jiro.CodeAnalysis.Core
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal sealed class StringFixProvider
+    {
+        private readonly CodeFixProvider codeFixProvider;
+        private readonly Document document;
+
+        public StringFixProvider(Document document, CodeFixProvider codeFixProvider)
+        {
+            Debug.Assert(document != null, "document must not be null");
+            Debug.Assert(codeFixProvider != null, "codeFixProvider must not be null");
+
+            this.document = document;
+            this.codeFixProvider = codeFixProvider;
+        }
+
+        internal async Task<ImmutableArray<string>> List(ImmutableArray<Diagnostic> diagnostics)
+        {
+            var actions = await 
+                ListCodeActions(this.codeFixProvider, this.document, diagnostics);
+
+            var changes = await
+                Task.WhenAll(actions.Select(x => ToString(x, this.document.Id)));
+
+            return changes.ToImmutableArray();
+        }
+
+        private static async Task<IEnumerable<CodeAction>> ListCodeActions(
+            CodeFixProvider codeFixProvider,
+            Document document,
+            ImmutableArray<Diagnostic> diagnostics)
+        {
+            var result = new ConcurrentBag<CodeAction>();
+
+            var tasks =
+                diagnostics
+                    .Select(x => ToCodeFixContext(x, document, result.Add))
+                    .Select(x => codeFixProvider.ComputeFixesAsync(x))
+                    .ToImmutableArray();
+
+            await Task.WhenAll(tasks);
+
+            return result;
+        }
+
+        private static CodeFixContext ToCodeFixContext(
+            Diagnostic diagnostic,
+            Document document,
+            Action<CodeAction> registration)
+        {
+            return new
+                CodeFixContext(
+                    document,
+                    diagnostic,
+                    (action, _) => registration(action),
+                    CancellationToken.None);
+        }
+
+        private static async Task<string> ToString(CodeAction codeAction, DocumentId documentId)
+        {
+            var text = await
+                (await GetApplyChangesOperation(codeAction))
+                    .ChangedSolution
+                    .GetDocument(documentId)
+                    .GetTextAsync();
+
+            return text.ToString();
+        }
+
+        private static async Task<ApplyChangesOperation> GetApplyChangesOperation(CodeAction codeAction)
+        {
+            var operations = await codeAction.GetOperationsAsync(CancellationToken.None);
+
+            return (ApplyChangesOperation)operations.Single();
+        }
+    }
+}
